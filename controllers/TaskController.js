679fd6401d0d0ff82fecb8bec948e491c7871c2f
Andre Luiz Polzin <andrelpolzin@gmail.com>
2026-01-02 15:00:15 -0300

    Almost done

--- controllers/TaskController.js
+++ controllers/TaskController.js
@@ -39,21 +39,34 @@ class TaskController {
 
 
     static async updateTask(req, res) {
+        const id = req.params.id;
 
+        try {
+            const task = await Task.findOne({where: {id: id}, raw: true})
+            // console.log('Task from db', task)
+            res.status(200).render('tasks/edit', { task })
+        } catch (error) {
+            console.error(error)
+            res.status(500).json({message: 'An error occurred, try again'})
+        }
     }
 
 
     static async updateTaskSave(req, res) {
-        const id = req.body.id
+        console.log('dentro de updateTaskSave')
+        const { id, title, description, done } = req.body
 
         const updatedTask = {
             title: title,
             description: description,
-            // done: 
+            done: done === '1' ? true : false
         }
+
+        console.log(updatedTask, id)
 
         try {
             await Task.update(updatedTask, {where: {id: id}})
+            res.status(200).redirect('/tasks')
         } catch (error) {
             console.error(error)
             res.status(500).json({message: 'An error occurred, try again'})
@@ -71,6 +84,24 @@ class TaskController {
             console.error(error)
             res.status(500).json({message: 'An error occurred, try again'})
         }
+    }
+
+
+    static async toggleStatus(req, res) {
+        const id = req.body.id
+
+        const task = {
+            done: req.body.done === 'false' ? true : false
+        }
+
+        try {
+            await Task.update(task, {where: {id: id}})
+            res.status(200).redirect('/tasks')
+        } catch (error) {
+            console.error(error)
+            res.status(500).json({message: 'An error occurred, try again'})
+        }
+
     }
 }
 
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -6,8 +6,9 @@ import TaskController from '../controllers/TaskController.js'
 router.get('/', TaskController.showTasks)
 router.get('/create', TaskController.createTask)
 router.post('/createSave', TaskController.createTaskSave)
-router.get('/edit', TaskController.updateTask)
-router.put('/editSave', TaskController.updateTaskSave)
+router.get('/edit/:id', TaskController.updateTask)
+router.post('/editSave', TaskController.updateTaskSave)
 router.post('/delete', TaskController.deleteTask)
+router.post('/toggleStatus', TaskController.toggleStatus)
 
 export default router
